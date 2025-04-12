dfb790ef868eadb651c7bdee5bc92735b31ec0d2
Charls <scjcharls@gmail.com>
2025-04-12 19:18:58 +0800

    All the things is about the practice of the day1 challenge, so just do the practice and comment some code.

---
+++ Day_1/main.py
@@ -0,0 +1,18 @@
+# print("Hello, World!")
+# input("What is your name?")
+
+# print("Hello, " + input("What is your name?"))
+
+# get the intput
+# data = input("Please input a string.")
+# caculate the len
+# data_len = len(data)
+# print the len
+# print(f"The len of the data is {data_len}")
+# print("The len of the data is %d" %data_len)
+
+the_city_name = input("Which city did you grow up in?")
+the_pet_name = input("What is ypur pet name?")
+
+print("The name we recommend is " + the_city_name + the_pet_name)
+
